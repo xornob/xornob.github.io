43bfaaee54008e35e8cde93b94004c7f2b460cc5
CJ Dvorak <xornob@users.noreply.github.com>
2018-07-19 13:03:54 -0600

    Overhaul wordsearch
    - Allow generating new search w/o refresh
    - Force 1 occurence of the word
    - cheat() mode
    - General better logic

--- wordsearch/wordsearch.js
+++ wordsearch/wordsearch.js
@@ -1,241 +1,319 @@
-var size = {x:20,y:20}, grid = {}, ss = {x:-1,y:-1}, sel = false, sh = [], wordcount=0, giveupletters=[], givenup=false, gottenwords={},
-container, word, drow, noletter, counts, total, remaining, giveup, wordgetter, xgetter, ygetter;
+var cheating = false, size = {x:20,y:20}, word, drow, grid = {}, wordcount=0, giveupletters=[], givenup=false, gottenwords={}, elem={}, noletter, settingErrors = []
+selection = {
+	head:{x:-1,y:-1},
+	list:[],
+	exists:false
+};
+const DEFAULT = {
+	SIZE:{x:20,y:20},
+	WORD:"fish",
+	ALLOWEDCHARS:" !\"#$%&'()*+,-./0123456789:;<=>?@[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+	SETTINGINFO:"Enter the settings to use for the word search"
+};
 
-function getletterdiv (o) {return container[o.x][o.y];}
-
-function getletterchar (o) {return grid[o.x][o.y];}
+function cheat () {cheating=true;}
 
 function c (a,b) {return {'x':a,'y':b}}
 
-function clearhighlight () {
-	while (sh.length) {
-		var z = getletterdiv(sh.shift());
-		z.style.backgroundColor=z.iw;
+function divGet (o) {
+	return ((((elem.container[o.x])||{})[o.y])||noletter);
+}
+
+function charGet (o) {
+	return ((((grid[o.x])||{})[o.y])||'-');
+}
+
+function highlightRemove () {
+	while (selection.list.length) {
+		var z = divGet(selection.list.shift());
+		z.style.backgroundColor=z.unselectedColor;
 	}
 }
 
-function highlight () {
-	for (var i = 0; i < sh.length; i++) {
-		getletterdiv(sh[i]).style.backgroundColor='red';
+function highlightAdd () {
+	for (var i = 0; i < selection.list.length; i++) {
+		divGet(selection.list[i]).style.backgroundColor='red';
 	}
 }
 
-function startselect (x,y) {
+function selectionBegin (x,y) {
 	if (givenup) return;
-	sel=true;
-	getletterdiv(c(x,y)).style.backgroundColor='red';
-	ss = c(x,y);
-	sh = [ss];
+	selection.exists=true;
+	divGet(c(x,y)).style.backgroundColor='red';
+	selection.head = c(x,y);
+	selection.list = [selection.head];
 }
 
-function selection (x,y) {
-	var bx=Math.max(x,ss.x);
-	var sx=Math.min(x,ss.x);
-	var by=Math.max(y,ss.y);
-	var sy=Math.min(y,ss.y);
-	clearhighlight();
+function selectionUpdate (x,y) {
+	var bx=Math.max(x,selection.head.x);
+	var sx=Math.min(x,selection.head.x);
+	var by=Math.max(y,selection.head.y);
+	var sy=Math.min(y,selection.head.y);
+	highlightRemove();
 	if (bx==sx) {
 		for (var i = sy; i <= by; i++) {
-			sh.push(c(bx,i));
+			selection.list.push(c(bx,i));
 		}
 	}
 	else if (by==sy) {
 		for (var i = sx; i <= bx; i++) {
-			sh.push(c(i,by));
+			selection.list.push(c(i,by));
 		}
 	}
-	else if ((y-ss.y)===(x-ss.x)) {
+	else if ((y-selection.head.y)===(x-selection.head.x)) {
 		for (var i = 0; i <= bx-sx; i++) {
-			sh.push(c(sx+i,sy+i))
+			selection.list.push(c(sx+i,sy+i))
 		};
 	}
-	else if ((y-ss.y)===(ss.x-x)) {
+	else if ((y-selection.head.y)===(selection.head.x-x)) {
 		for (var i = 0; i <= bx-sx; i++) {
-			sh.push(c(sx+i,by-i))
+			selection.list.push(c(sx+i,by-i))
 		};
 	}
-	else sh = [ss];
-	highlight();
+	else selection.list = [selection.head];
+	highlightAdd();
 }
 
-function stopselect () {
-	sel=false;
-	var s = '';
-	for (var i = 0; i < sh.length; i++) {
-		s+=getletterchar(sh[i]);
-	};
-	if ((s===word || s===drow) && gottenwords[''+sh[0].x+sh[0].y+sh[word.length-1].x+sh[word.length-1].y]!=='lol') {
-		for (var i = 0; i < sh.length; i++) {
-			getletterdiv(sh[i]).iw='#69f';
+function selectionStop () {
+	if (selection.list.length == word.length) {
+		var s = '';
+		for (var i = 0; i < selection.list.length; i++) {
+			s+=charGet(selection.list[i]);
 		};
-		gottenwords[''+sh[0].x+sh[0].y+sh[word.length-1].x+sh[word.length-1].y]='lol';
-		wordcount -= 1;
-		remaining.innerHTML = wordcount;
-		if (wordcount===0) {
-			counts.innerHTML='You got all '+total.innerHTML+' occurences of "'+word+'"<br>Refresh to try again';
-			givenup=true;
-			document.body.removeChild(giveup);
+		if ((s===word || s===drow) && !gottenwords[''+selection.list[0].x+selection.list[0].y+selection.list[word.length-1].x+selection.list[word.length-1].y]) {
+			for (var i = 0; i < selection.list.length; i++) {
+				divGet(selection.list[i]).unselectedColor='#69f';
+			};
+			gottenwords[''+selection.list[0].x+selection.list[0].y+selection.list[word.length-1].x+selection.list[word.length-1].y]=true;
+			wordcount -= 1;
+			elem.remaining.set(wordcount);
+			if (wordcount===0) {
+				elem.complete.set('You got all '+total.innerHTML+' occurences of "'+word+'"<br>Refresh to try again');
+				elem.complete.style.display='';
+				givenup=true;
+				elem.giveup.style.display='none';
+			};
 		};
-	};
-	clearhighlight();
+	}
+	selection.exists=false;
+	highlightRemove();
 }
 
 function start () {
-	word = wordgetter.value;
-	counts.innerHTML='';
-	if (!isNaN(xgetter.value) && !isNaN(ygetter.value) && xgetter.value && ygetter.value) {
-		size.x = +xgetter.value;
-		size.y = +ygetter.value;
+	word = elem.wordin.value;
+	if (!isNaN(elem.x.value) && !isNaN(elem.y.value) && elem.x.value && elem.y.value) {
+		size.x = +elem.x.value;
+		size.y = +elem.y.value;
 	} else return;
 	drow = word.split('').reverse().join('');
-	document.body.removeChild(document.getElementById('toDelete'));
-	for (var i = 0; i < size.x; i++) {
+	elem.settings.style.display="none";
+	for (var i = 0; i < size.x; i++) { //Generate Grid of Chars
 		grid[i]={};
 		for (var j = 0; j < size.y; j++) {
 			grid[i][j]=word.charAt(Math.random()*word.length);
 		}
 	}
-	for (var x = 0, y = 0; y < size.y; x++) {
-		if (y===0) {
-			var temp = document.createElement('div');
-			temp.className='col';
-			temp.x=x;
-			container.appendChild(temp);
-			container[x]=temp;
-		}
+	switch (cheating?1:(Math.floor(Math.random()*4))) { //Force 1 occurrence of word
+		case 0: //Vertical
+			temp={x:(Math.floor(Math.random()*(size.x))),y:(Math.floor(Math.random()*(size.y-word.length))),word:(Math.random()<.5?word:drow)};
+			for (i=0;i<word.length;i++) {
+				grid[temp.x][temp.y+i]=temp.word.charAt(i);
+			}
+			break;
+		case 1: //Horizontal
+			temp=cheating?{x:0,y:0,word:word}:({x:(Math.floor(Math.random()*(size.x-word.length))),y:(Math.floor(Math.random()*(size.y))),word:(Math.random()<.5?word:drow)});
+			for (i=0;i<word.length;i++) {
+				grid[temp.x+i][temp.y]=temp.word.charAt(i);
+			}
+			break;
+		case 2: //Diagonal \ 
+			temp={x:(Math.floor(Math.random()*(size.x-word.length))),y:(Math.floor(Math.random()*(size.y-word.length))),word:(Math.random()<.5?word:drow)};
+			for (i=0;i<word.length;i++) {
+				grid[temp.x+i][temp.y+i]=temp.word.charAt(i);
+			}
+			break;
+		case 3: //Diagonal /
+			temp={x:(Math.floor(Math.random()*(size.x-word.length))),y:(Math.floor(Math.random()*(size.y-word.length))),word:(Math.random()<.5?word:drow)};
+			for (i=0;i<word.length;i++) {
+				grid[temp.x+word.length-(i+1)][temp.y+i]=temp.word.charAt(i);
+			}
+			break;
+	}
+	for (var x = 0; x < size.x; x++) { //Construct search HTML elements
 		var temp = document.createElement('div');
+		temp.className='col';
 		temp.x=x;
-		temp.y=y;
-		temp.iw='transparent';
-		temp.style.backgroundColor=temp.iw;
-		temp.onmousedown = function () {startselect(this.x,this.y);};
-		temp.onmouseenter = function () {
-			if (sel) {
-				selection(this.x,this.y);
+		elem.container.appendChild(temp);
+		elem.container[x]=temp;
+		for (var y = 0; y < size.y; y++) {
+			var temp = document.createElement('div');
+			temp.x=x;
+			temp.y=y;
+			temp.unselectedColor='transparent';
+			temp.style.backgroundColor=temp.unselectedColor;
+			temp.onmousedown = function () {selectionBegin(this.x,this.y);};
+			temp.onmouseenter = function () {
+				if (selection.exists) {
+					selectionUpdate(this.x,this.y);
+				}
+			};
+			temp.onmouseup = function() {selectionStop();};
+			temp.className='letter';
+			temp.innerHTML=charGet(c(x,y));
+			elem.container[x].appendChild(temp);
+			elem.container[x][y]=temp;
+		}
+	}
+	for (x = 0; x < size.x; x++) { //Finds Words Vertical
+		for (y = 0; y < size.y; y++) {
+			var s = '';
+			for (var i = 0; i < word.length; i++) {
+				s+=charGet(c(x,y+i));
+			};
+			if (s===word||s===drow) {
+				wordcount+=1;
+				gottenwords[""+x+y+x+(y+word.length)]=false;
+				for (var i = 0; i < word.length; i++) {
+					giveupletters.push(divGet(c(x,y+i)));
+					if (cheating) divGet(c(x,y+i)).style.fontWeight = 600;
+				};
 			}
 		};
-		temp.onmouseup = function() {stopselect();};
-		temp.className='letter';
-		temp.innerHTML=grid[x][y];
-		container[x].appendChild(temp);
-		container[x][y]=temp;
-		if (x==size.x-1) {x=-1;y++;}
-	}
-	for (var x = 0, y = 0; y < size.y - word.length; x++) { //Vertical
-		var s = '';
-		for (var i = 0; i < word.length; i++) {
-			s+=getletterchar(c(x,y+i));
-		};
-		if (s===word||s===drow) {
-			wordcount+=1;
-			for (var i = 0; i < word.length; i++) {
-				giveupletters.push(getletterdiv(c(x,y+i)));
-			};
-		}
-		if (x==size.x-1) {x=-1;y++;}
 	};
-	for (var y = 0, x = 0; x < size.x-word.length; y++) { //Horizontal
-		var s = '';
-		for (var i = 0; i < word.length; i++) {
-			s+=getletterchar(c(x+i,y));
-		};
-		if (s===word||s===drow) {
-			wordcount+=1;
+	for (x = 0; x < size.x; x++) { //Finds Words Horizontal
+		for (y = 0; y < size.y; y++) {
+			var s = '';
 			for (var i = 0; i < word.length; i++) {
-				giveupletters.push(getletterdiv(c(x+i,y)));
+				s+=charGet(c(x+i,y));
 			};
-		}
-		if (y==size.y-1) {y=-1;x++;}
-	};
-	for (var y = 0, x = 0; x < size.x-word.length; y++) { // Diagonal \
-		var s = '';
-		for (var i = 0; i < word.length; i++) {
-			s+=getletterchar(c(x+i,y+i));
+			if (s===word||s===drow) {
+				wordcount+=1;
+				gottenwords[""+x+y+(x+word.length)+y]=false;
+				for (var i = 0; i < word.length; i++) {
+					giveupletters.push(divGet(c(x+i,y)));
+					if (cheating) divGet(c(x+i,y)).style.fontWeight = 600;
+				};
+			}
 		};
-		if (s===word||s===drow) {
-			wordcount+=1;
-			for (var i = 0; i < word.length; i++) {
-				giveupletters.push(getletterdiv(c(x+i,y+i)));
-			};
-		}
-		if (y==size.y-(1+word.length)) {y=-1;x++;}
 	};
-	for (var x = 0, y = word.length; x < size.x-word.length; y++) { // Diagonal /
-		var s = '';
-		for (var i = 0; i < word.length; i++) {
-			s+=getletterchar(c(x+i,y-i));
+	for (x = 0; x < size.x; x++) { //Finds Words Diagonal \ 
+		for (y = 0; y < size.y; y++) {
+			var s = '';
+			for (var i = 0; i < word.length; i++) {
+				s+=charGet(c(x+i,y+i));
+			};
+			if (s===word||s===drow) {
+				wordcount+=1;
+				gottenwords[""+x+y+(x+word.length)+(y+word.length)]=false;
+				for (var i = 0; i < word.length; i++) {
+					giveupletters.push(divGet(c(x+i,y+i)));
+					if (cheating) divGet(c(x+i,y+i)).style.fontWeight = 600;
+				};
+			}
 		};
-		if (s===word||s===drow) {
-			wordcount+=1;
-			for (var i = 0; i < word.length; i++) {
-				giveupletters.push(getletterdiv(c(x+i,y-i)));
-			};
-		}
-		if (y==size.y-(1+word.length)) {y=word.length-1;x++;}
 	};
-	counts.innerHTML='There are <span id="remaining">'+wordcount+'</span>/<span id="total">'+wordcount+'</span> occurences of "'+word+'" remaining';
-	total=document.getElementById('total');
-	remaining=document.getElementById('remaining');
-	giveup = document.createElement('button');
-	giveup.innerHTML='Give Up';
-	giveup.onmousedown = function () {
+	for (x = 0; x < size.x; x++) { //Finds Words Diagonal /
+		for (y = 0; y < size.y; y++) {
+			var s = '';
+			for (var i = 0; i < word.length; i++) {
+				s+=charGet(c(x+i,y-i));
+			};
+			if (s===word||s===drow) {
+				wordcount+=1;
+				for (var i = 0; i < word.length; i++) {
+					giveupletters.push(divGet(c(x+i,y-i)));
+					if (cheating) divGet(c(x+i,y-i)).style.fontWeight = 600;
+				};
+			}
+		};
+	};
+	elem.remaining.set(elem.total.set(wordcount));
+	elem.wordDisplay.set('"'+word+'"');
+	elem.giveup.set('Give Up');
+	elem.giveup.onmousedown = function () {
 		this.innerHTML='Are you SURE?';
 		this.onmousedown= function () {
 			givenup=true;
-			counts.innerHTML='YOU GAVE UP WHEN THERE WERE '+wordcount+'/'+total.innerHTML+' OCCURENCES REMAINING<br>Refresh to try again';
+			elem.givenup.set('YOU GAVE UP WHEN THERE WERE '+wordcount+'/'+total.innerHTML+' OCCURENCES REMAINING');
+			elem.counts.style.display='none';
+			elem.givenup.style.display='';
 			for (var i = 0; i < giveupletters.length; i++) {
-				if (giveupletters[i].iw==='transparent') giveupletters[i].style.backgroundColor='#f69';
+				if (giveupletters[i].unselectedColor==='transparent') giveupletters[i].style.backgroundColor='#f69';
 			};
-			document.body.removeChild(giveup);
+			elem.giveup.style.display='none';
 		}
 	}
-	document.body.appendChild(giveup);
+	elem.searchInfo.style.display = "";
+}
+
+function newSearch () {
+	for (var x = 0; x < size.x; x++) {
+		elem.container.removeChild(elem.container[x]);
+	}	
+	elem.searchInfo.style.display='none';
+	elem.counts.style.display='';
+	elem.giveup.style.display='';
+	elem.complete.style.display='none';
+	elem.givenup.style.display='none';
+	elem.settings.style.display='';
+	wordcount=0;
+	giveupletters=[];
+	givenup=false;
+	gottenwords={};
+}
+
+function authSettings() {
+	return;
 }
 
 window.onload = function() {
 	document.body.removeChild(document.getElementById('nojs'));
-	document.getElementById('start').disabled=false;
-	document.getElementById('start').onmousedown=start;
-	container = document.getElementById('container');
-	noletter = document.createElement('div');
-	counts = document.getElementById('counts');
-	wordgetter = document.getElementById('wordgetter');
-	xgetter = document.getElementById('x');
-	ygetter = document.getElementById('y');
-	container[-1]={'-1':noletter};
-	wordgetter.oninput=function() {
-		var s='', o=this.value.split('');
+	elem.start = document.getElementById('start');
+	elem.start.disabled=false;
+	elem.start.onmousedown=start;
+	elem.container = document.getElementById('container');
+	elem.noletter = document.createElement('div');
+	elem.counts = document.getElementById('counts');
+	elem.wordin = document.getElementById('wordin');
+	elem.x = document.getElementById('x');
+	elem.y = document.getElementById('y');
+	elem.total=document.getElementById('total');
+	elem.remaining = document.getElementById('remaining');
+	elem.wordDisplay = document.getElementById('wordDisplay');
+	elem.complete = document.getElementById('complete');
+	elem.giveup = document.getElementById('giveup');
+	elem.searchInfo = document.getElementById('searchInfo');
+	elem.settingInfo = document.getElementById('settingInfo');
+	elem.settings = document.getElementById('settings');
+	elem.restart = document.getElementById('restart');
+	elem.givenup = document.getElementById('givenup');
+	elem.restart.onmousedown = newSearch;
+	elem.givenup.set = elem.wordin.set = elem.x.set = elem.y.set = elem.total.set = elem.remaining.set = elem.wordDisplay.set = elem.complete.set = elem.giveup.set = elem.searchInfo.set = elem.settingInfo.set = function(s) {return this.innerHTML = this.value = s;};
+	elem.wordin.oninput=function() {
+		var s='', o=this.value.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").split('');
 		for (var i = 0; i < o.length; i++) {
-			if (o[i].match(/[a-z]/i)) s+=o[i];
+			if (DEFAULT.ALLOWEDCHARS.includes(o[i])) s+=o[i];
 		};
-		this.value=s.toLowerCase();
-		document.getElementById('start').innerHTML='Start';
+		this.value=s;
+		authSettings();
 	};
-	wordgetter.onchange=wordgetter.oninput;
-	wordgetter.onpropertychange=wordgetter.oninput;
-	xgetter.oninput = function() {
+	elem.wordin.onchange=elem.wordin.oninput;
+	elem.wordin.onpropertychange=elem.wordin.oninput;
+	elem.x.onpropertychange=elem.x.onchange=elem.x.oninput = function() {
 		var s='', o=this.value.split('');
 		for (var i = 0; i < o.length; i++) {
 			if (o[i].match(/[0-9]/i)) s+=o[i];
 		};
-		if (s==='') counts.innerHTML='The width can\'t be empty<br>';
-		else if (+s===0) counts.innerHTML='The width can\'t be 0';
-		else counts.innerHTML='Enter a word to use for the word search';
 		this.value=s;
+		authSettings();
 	};
-	xgetter.onchange=xgetter.oninput;
-	xgetter.onpropertychange=xgetter.oninput;
-	ygetter.oninput = function() {
+	elem.y.onpropertychange=elem.y.onchange=elem.y.oninput = function() {
 		var s='', o=this.value.split('');
 		for (var i = 0; i < o.length; i++) {
 			if (o[i].match(/[0-9]/i)) s+=o[i];
 		};
-		if (s==='') counts.innerHTML='The height can\'t be empty<br>';
-		else if (+s===0) counts.innerHTML='The height can\'t be 0';
-		else counts.innerHTML='Enter a word to use for the word search';
 		this.value=s;
+		authSettings();
 	};
-	ygetter.onchange=ygetter.oninput;
-	ygetter.onpropertychange=ygetter.oninput;
-	document.body.onmouseup=function() {sel=false;clearhighlight();};;
+	document.body.onmouseup=function() {selection.exists=false;highlightRemove();};;
 };
